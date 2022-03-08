ad700743d9c5ddee51f0efa82d0a8f7dd8e70689
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-09 00:47:53 +0900

    descriptionカラムに文字数制限を設置

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_one :user, through: :book_user
 
   validates :title, presence: true
+  validates :description, length: { maximum: 300 }
   validates :youtube_id,  presence: true,
                           length: { is: 11, allow_blank: true, message: "URL is not youtube video's URL"}
 end
