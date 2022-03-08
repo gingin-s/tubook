b65375a212adbd15f26dba7e409720917f867ab5
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-08 18:04:44 +0900

    Revert "createアクションの定義"
    This reverts commit 340d82ac48de22a5494d6aedba6f4ecf6833bfbc.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,8 @@
 class BooksController < ApplicationController
   def new
     @user = User.find(current_user.id)
-    @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to root_path
-    else
-      render :new
   end
 end
