fef25bf95ff09118a879c9c97bde62c93e2efdd2
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-08 18:13:45 +0900

    ストロングパラメーターの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,21 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = Book.new(user_book_params)
     if @book.save
       redirect_to root_path
     else
       render :new
+  end
+
+  private
+
+  def user_book_params
+    params.require(:book).permit(:title, :description).merge(user_id: current_user.id, youtube_id: get_youtube_id)
+  end
+
+  def get_youtube_id
+    url = params.require(:book).permit(:youtube_id)
+    url.last(11)
   end
 end
