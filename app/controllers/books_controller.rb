340d82ac48de22a5494d6aedba6f4ecf6833bfbc
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-08 18:03:21 +0900

    createアクションの定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,14 @@
 class BooksController < ApplicationController
   def new
     @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
   end
 end
