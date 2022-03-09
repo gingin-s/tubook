701839646dbd7f33ffbf1156aa51426e43323538
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-09 12:03:53 +0900

    youtube動画の埋め込み・簡易viewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   resources :users, only: :update
   resources :books, only: [:new, :create]
+  resources :books, only: [:edit] do
+    resources :notes, only: :create
+  end
 end
