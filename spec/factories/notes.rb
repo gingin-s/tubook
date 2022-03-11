35f285df15998e8386da6e2eb05b02f27aa7ed04
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-11 22:29:47 +0900

    noteモデル単体テスト

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :note do
+    text { Faker::Lorem.paragraph }
+    video_time { Faker::Number.number(digits: 3) }
+    
+    association :user
+    association :book
   end
 end
