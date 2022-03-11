bf6156dbaa051ca54799947fb7d547af8911c72a
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-11 22:05:42 +0900

    textカラムに文字数制限

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,6 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :text, presence: true
+  validates :text, presence: true, length: { maximum: 300 }
   validates :video_time, presence: true
 end
