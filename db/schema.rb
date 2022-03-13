c971de6011baf9b2f383a7a14e655829fdd046f3
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-13 19:11:01 +0900

    room・room_usersモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_023249) do
+ActiveRecord::Schema.define(version: 2022_03_13_100938) do
 
   create_table "book_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "book_id", null: false
@@ -40,6 +40,21 @@ ActiveRecord::Schema.define(version: 2022_03_09_023249) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -59,4 +74,6 @@ ActiveRecord::Schema.define(version: 2022_03_09_023249) do
   add_foreign_key "book_users", "users"
   add_foreign_key "notes", "books"
   add_foreign_key "notes", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
