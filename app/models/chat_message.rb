8bee7a9b5f72c22197bf9413f2018e0de090a70d
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-20 18:15:59 +0900

    chat_messageモデルのバリデーション

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -2,6 +2,8 @@ class ChatMessage < ApplicationRecord
   belongs_to :room
   belongs_to :user
 
+  validates :text, presence: true
+
   def template
     ApplicationController.renderer.render partial: 'shared/left_message', locals: { message: self }
   end
