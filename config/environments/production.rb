43ff91d6a0f752e90341f780d2a699a21e8c0902
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-20 17:52:01 +0900

    Action Cableの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://tubook.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://tubook.herokuapp.com', 'http://tubook.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
