1f322a41e2719f567fecb9a4a168217e26021309
gingin-s <ginga.capriccioso823@gmail.com>
2022-03-20 20:04:30 +0900

    carrierwave.rb修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ unless Rails.env.development? || Rails.env.test?
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-      region: 'ap-northeast-1'
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
     }
 
     config.fog_directory  = 'tubook'
